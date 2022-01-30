82ac9ed1f95f8ce0dd21de6d5bc5cccad9f87779
Lars Grimstad <lgrimstad@sagarobotics.com>
2022-01-30 21:43:38 +0100

    housekeeping

--- scripts/02_if_and_user_input.sh
+++ scripts/02_if_and_user_input.sh
@@ -2,14 +2,14 @@
 
 echo "Let's compare numbers"
 echo "enter number a"
-read num_a
+read -r num_a
 echo "enter number b"
-read num_b
+read -r num_b
 
 
-if [ $num_a -gt $num_b ]; then
+if [ "$num_a" -gt "$num_b" ]; then
     echo "a is greater than b"
-elif [ $num_a -eq $num_b ]; then
+elif [ "$num_a" -eq "$num_b" ]; then
     echo "a is equal to b"
 else
     echo "a is not greater than b"	
@@ -19,12 +19,12 @@ fi
 echo -e "\nLet's compare strings"
 
 echo "enter word a"
-read word_a
+read -r word_a
 echo "enter word b"
-read word_b
+read -r word_b
 
 
-if [ $word_a = $word_b ]; then
+if [ "$word_a" = "$word_b" ]; then
     echo "word a is equal to word b"
 else
     echo "word a is not equal to word b"
