14a5d634216a4370c98b3c9e8ca2d05477b2db37
Lars Grimstad <lgrimstad@sagarobotics.com>
2022-01-30 22:40:48 +0100

    housekeeping

--- scripts/06_tar_and_compress.sh
+++ scripts/06_tar_and_compress.sh
@@ -3,7 +3,7 @@
 
 # Let's create a new directory with some dummy files so that we can demo tar
 dir_name="$(date +%H%M%S)_demo"
-mkdir $dir_name
+mkdir "$dir_name"
 
 for i in {0000..1000}; do
     echo "I am file number $i" > "${dir_name}/file_$i"
@@ -16,27 +16,27 @@ done
 
 # tarball without compression
 
-tar -cf ${dir_name}_tar_example.tar $dir_name
+tar -cf "${dir_name}_tar_example.tar" "$dir_name"
 
 
 
 
 # tarball with gzip compression
 
-tar -czf ${dir_name}_targz_example.tar.gz $dir_name
+tar -czf "${dir_name}_targz_example.tar.gz" "$dir_name"
 
 
 
 
 # tarball with XZ compression (this is the most modern and best option)
 
-tar -cJf ${dir_name}_tarxz_example.tar.xz $dir_name
+tar -cJf "${dir_name}_tarxz_example.tar.xz" "$dir_name"
 
 
 
 
 # for zip files we use the zip and unzip commands
-zip -rq ${dir_name}_zip_example $dir_name
+zip -rq "${dir_name}_zip_example" "$dir_name"
 
 
 
@@ -58,22 +58,22 @@ zip -rq ${dir_name}_zip_example $dir_name
 # unzip <filename>
 
 
-tar -xf ${dir_name}_tar_example.tar --one-top-level
+tar -xf "${dir_name}_tar_example.tar" --one-top-level
 
 
-tar -xf ${dir_name}_targz_example.tar.gz --one-top-level 
+tar -xf "${dir_name}_targz_example.tar.gz" --one-top-level 
 
 
-tar -xf ${dir_name}_tarxz_example.tar.xz --one-top-level
+tar -xf "${dir_name}_tarxz_example.tar.xz" --one-top-level
 
 
-unzip -qq -d ${dir_name}_zip_example ${dir_name}_zip_example.zip
+unzip -qq -d "${dir_name}_zip_example" "${dir_name}_zip_example.zip"
 
 
 # Now let's look at the size of the original folder compared to the compressed ones using the du tool.
 
-du -b ${dir_name} # 4096*9 is the folder itself, the rest is the content
-du -b ${dir_name}_tar_example.tar
-du -b ${dir_name}_targz_example.tar.gz
-du -b ${dir_name}_tarxz_example.tar.xz
-du -b ${dir_name}_zip_example.zip
+du -b "${dir_name}" # 4096*9 is the folder itself, the rest is the content
+du -b "${dir_name}_tar_example.tar"
+du -b "${dir_name}_targz_example.tar.gz"
+du -b "${dir_name}_tarxz_example.tar.xz"
+du -b "${dir_name}_zip_example.zip"
